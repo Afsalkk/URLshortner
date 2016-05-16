328ce067739d591f8c9d830995e8156ee6527011
sajeerashamlan <sajeerakt@gmail.com>
2016-05-16 16:13:50 +0530

    app blade layout.

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!-- resources/views/layouts/app.blade.php -->
+
+<!DOCTYPE html>
+<html lang="en"> 
+    <head>
+        <title>Url Shortner</title>
+
+        <!-- css and javascript -->
+    </head>  
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <!-- navbar contents -->
+            </nav>
+        </div>
+
+        @yield ('content')
+    </body>
+</html>
